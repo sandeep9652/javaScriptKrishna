52b953eb0d0715628ad70c55d1d13c7d59643e3f
sandeep9652 <mupidiwar@gmail.com>
2025-01-12 03:46:11 +0530

    charecter_occurence

---
+++ charoccurence.js
@@ -0,0 +1,15 @@
+function countCharacterOccurrences(str) {
+    const charCount = {};
+
+    for (let char of str) {
+        charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+    }
+
+    return charCount;
+}
+
+const result = countCharacterOccurrences('hello world');
+console.log(result);
+console.log(result);
+
+
